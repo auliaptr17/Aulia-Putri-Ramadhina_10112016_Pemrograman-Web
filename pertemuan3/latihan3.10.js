be32ee81360c7b1aedfeae9b655d40f84bc261d7
auliaptr17 <auliaputrir1706@gmail.com>
2026-09-09 11:57:23 +0700

    Add files via upload

---
+++ pertemuan3/latihan3.10.js
@@ -0,0 +1,9 @@
+//buat variabel Number angka dan isi dengan nilai 17
+var angka = 17;
+
+// jika sisa hasil bagi (mod) variabel angka dengan 2 adalah 0,
+// maka jalankan perintah console.log()
+if (angka % 2 == 0)
+    console.log("Angka adalah bilangan genap");
+else
+    console.log("Angka adalah bilangan ganjil");
---
+++ pertemuan3/latihan3.11.js
@@ -0,0 +1,20 @@
+<script>
+var angka=2;
+
+if (angka==1)
+{
+console.log("Angka Satu");
+}
+else if (angka==2)
+{
+console.log("Angka Dua");
+}
+else if (angka==3)
+{
+console.log("Angka Tiga");
+}
+else if (angka==4)
+{
+console.log("Angka Empat");
+}
+</script>
